5e508bd62904c52c5bdc6723f67eb4940eac2065
Dan Royer <dan@marginallyclever.com>
2017-12-23 18:26:45 -0800

    added MJPEG stream capture & unit test

--- src/com/TwitchJigsawRobotController/MapMaker.java
+++ src/com/TwitchJigsawRobotController/MapMaker.java
@@ -1,91 +1,136 @@
 package com.TwitchJigsawRobotController;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.nio.ByteBuffer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URL;
+import java.net.URLConnection;
 
 import javax.imageio.ImageIO;
 
-import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_videoio.VideoCapture;
 import org.junit.Test;
-import org.opencv.core.Core;
 
 /**
- * Frame grabbing from examples at http://jcodec.org/
+ * http://thistleshrub.net/www/index.php?controller=posts&action=show&id=2012-05-13DisplayingStreamedMJPEGinJava.txt
+ * was a good start, but it assumed things about the shape of the header format that were not true for raspi.
  * @author Dan
  *
  */
 public class MapMaker {
+	private static final String STREAM_ADDRESS = "http://192.168.0.100:12345/?action=stream";
+	private static final String CONTENT_LENGTH = "Content-Length: ";
+	private static final String CONTENT_TYPE = "Content-Type: image/jpeg";
 
-	public static BufferedImage mat2Img(Mat matrix) {
-	    int cols = matrix.cols();
-	    int rows = matrix.rows();
-	    //int elemSize = (int)matrix.elemSize();
-	    //byte[] data = new byte[cols * rows * elemSize];
-	    int type;
-
-	    ByteBuffer buf = matrix.asByteBuffer();
-	    byte[] data = buf.array();
-	    
-	    switch (matrix.channels()) {
-	        case 1:
-	            type = BufferedImage.TYPE_BYTE_GRAY;
-	            break;
-
-	        case 3: 
-	            type = BufferedImage.TYPE_3BYTE_BGR;
-
-	            // bgr to rgb
-	            byte b;
-	            for(int i=0; i<data.length; i=i+3) {
-	                b = data[i];
-	                data[i] = data[i+2];
-	                data[i+2] = b;
-	            }
-	            break;
-
-	        default:
-	            return null;
-	    }
-
-	    BufferedImage image = new BufferedImage(cols, rows, type);
-	    image.getRaster().setDataElements(0, 0, cols, rows, data);
-
-	    return image;
-    } 
-	
-	public void takePicture(String outputFilename,String outputFileFormat) {
-		String STREAM_ADDRESS = "http://192.168.0.100:12345/?action=stream";
-		
-		System.out.println("Loading "+Core.NATIVE_LIBRARY_NAME);
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		
+	public void takeMJPEGFrameCapture(String outputFilename,String outputFileFormat) {
+		//System.out.println("takeMJPEGFrameCapture");
 		try {
-		    VideoCapture capture = new VideoCapture();
-	    	System.out.println("Opening "+STREAM_ADDRESS+"...");
-		    if(capture.open(STREAM_ADDRESS)) {
-		    	System.out.println("Reading frame...");
-		    	Mat mat=new Mat();
-		    	capture.read(mat);
-		    	System.out.println("Converting...");
-		    	BufferedImage bufferedImage = mat2Img(mat);
-		    	if(bufferedImage!=null) {
-			    	File outputFile = new File(outputFilename+"."+outputFileFormat);
-			    	System.out.println("Saved to "+outputFile.getAbsoluteFile());
-		    		ImageIO.write(bufferedImage, outputFileFormat, outputFile);
-		    	}
-		    }
-	    	System.out.println("Closing...");
-		    capture.release();
+			//System.out.println("connecting");
+			URL url = new URL(STREAM_ADDRESS);
+			URLConnection urlConn = url.openConnection();
+			urlConn.setReadTimeout(1000);
+			urlConn.connect();
 
+			//System.out.println("get stream");
+			InputStream urlStream = urlConn.getInputStream();
+
+			//System.out.println("retrieve image");
+			byte [] imageBytes = retrieveNextImage(urlStream);
+			//System.out.println("get byte array");
+			ByteArrayInputStream bais = new ByteArrayInputStream(imageBytes);
+			BufferedImage bufferedImage = ImageIO.read(bais);
+	    	if(bufferedImage!=null) {
+				//System.out.println("saving");
+		    	File outputFile = new File(outputFilename+"."+outputFileFormat);
+		    	//System.out.println("Saved to "+outputFile.getAbsoluteFile());
+	    		ImageIO.write(bufferedImage, outputFileFormat, outputFile);
+	    	}
+			//System.out.println("closing");
+	    	urlStream.close();
 		} catch(Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//System.out.println("done");
+	}
+	
+	/**
+	 * Using the urlStream get the next JPEG image as a byte[]
+	 * @return byte[] of the JPEG
+	 * @throws IOException
+	 */
+	private byte[] retrieveNextImage(InputStream urlStream) throws IOException {
+		StringWriter stringWriter = new StringWriter(256);
+		
+		boolean haveHeader = false; 
+		int currByte = -1;
+
+		String header = null;
+		// collect headers
+		while((currByte = urlStream.read()) > -1 && !haveHeader) {
+			stringWriter.write(currByte);
+			//System.out.print((char)currByte);
+			
+			String tempString = stringWriter.toString(); 
+			if(currByte=='\n') {
+				int indexOfType = tempString.indexOf(CONTENT_TYPE);
+				int indexOfLength = tempString.indexOf(CONTENT_LENGTH);
+				if(indexOfType > 0 && indexOfLength > 0) {
+					haveHeader = true;
+					header = tempString;
+				}
+			}
+		}		
+		
+		// 255 indicates the start of the jpeg image
+		while((urlStream.read()) != 255) {
+			// just skip extras
+			//System.out.print((char)i);
+		}
+
+		// rest is the buffer
+		int contentLength = contentLength(header);
+		byte[] imageBytes = new byte[contentLength + 1];
+		// since we ate the original 255 , shove it back in
+		imageBytes[0] = (byte)255;
+		int offset = 1;
+		int numRead = 0;
+		while (offset < imageBytes.length
+			&& (numRead=urlStream.read(imageBytes, offset, imageBytes.length-offset)) >= 0) {
+			offset += numRead;
+		}       
+		
+		return imageBytes;
+	}
+
+	// dirty but it works content-length parsing
+	private static int contentLength(String header)
+	{
+		int indexOfContentLength = header.indexOf(CONTENT_LENGTH);
+		int valueStartPos = indexOfContentLength + CONTENT_LENGTH.length();
+		int indexOfEOL = header.indexOf('\n', indexOfContentLength);
+		
+		String lengthValStr = header.substring(valueStartPos, indexOfEOL).trim();
+		
+		int retValue = Integer.parseInt(lengthValStr);
+		
+		return retValue;
+	}
+/*
+	@Test
+	public void testVideoCapture() {
+		takeVideoCapture("test1","png");
 	}
 	
 	@Test
-	public void grabOneFrame() {
-		takePicture("test","png");
+	public void testFFmegFrameGrabber() {
+		takeFFmpegFrame("test2","png");
+	}
+*/
+	@Test
+	public void testMJPEGCapture() {
+		takeMJPEGFrameCapture("test3","png");
 	}
 }
